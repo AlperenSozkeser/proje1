81139884aacfbe74a2a8292f33ba71b6a5997578
Alperen <alperencemalsozkeser@gmail.com>
2021-06-15 22:47:36 +0200

    proje1

---
+++ src/header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  button: {
+    color: "white",
+    fontSize: "15px",
+    fontWeight: "bold",
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <Typography variant="h6" className={classes.title}>
+            ACS BANK
+          </Typography>
+          <Button className={classes.button}>ABOUT US</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
